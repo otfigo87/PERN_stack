33b23efb5fab4513fc2e225646575c03b9513241
Otmane Aatik <aaotmane@gmail.com>
2023-05-17 13:59:16 -0400

    get all and post new

--- server/index.js
+++ server/index.js
@@ -7,9 +7,36 @@ const pool = require("./db");
 
 //middleware
 app.use(cors());
-app.use(express.json());// To get data from the request (from user/client side)
+app.use(express.json()); //To get data from the request (from user/client side) = req.body
 
 //Routes
+//create todo
+app.post("/todos", async (req, res) => {
+  try {
+    const description = req.body.description;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *", // RETURNING * : return the newly inserted row
+      [description]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+//get all todo
+app.get("/todos", async(req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+//update todo
+app.put("/:id", function (req, res) {});
+//delete todo
+app.delete("/:id", function (req, res) {});
 
 app.listen(port, () => {
   console.log(`Express server running on port ${port}`);
