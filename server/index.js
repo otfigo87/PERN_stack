277509165f4cb0d02c3a926d5055bd5e84a33dcc
Otmane Aatik <aaotmane@gmail.com>
2023-05-17 14:36:17 -0400

    update and delete routes done

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const port = 3001;
 const cors = require("cors");
 //Connect to Postgres
 const pool = require("./db");
+const { log } = require("console");
 
 //middleware
 app.use(cors());
@@ -40,7 +41,7 @@ app.get("/todos/:id", async(req,res) => {
     try {
         const {id} = req.params;
         const theTodo = await pool.query("SELECT * From todo WHERE todo_id = $1",
-         [id] ); // the $1 will be replaced with what in the []
+         [id] ); // the $1 will be replaced with variable inside the []
          res.json(theTodo.rows[0])
     } catch (error) {
         console.log(error.message)
@@ -48,9 +49,28 @@ app.get("/todos/:id", async(req,res) => {
 })
 
 //update todo
-app.put("/:id", function (req, res) {});
+app.put("/todos/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const {description} = req.body;
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE TODO_ID = $2",[description, id]);
+
+        res.json("todo was updated!");
+    } catch (error) {
+        console.log(error.message)
+    }
+});
 //delete todo
-app.delete("/:id", function (req, res) {});
+app.delete("/todos/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+        res.json("Todo was deleted!");
+    } catch (error) {
+        console.log(error.message)
+    }
+});
+
 
 app.listen(port, () => {
   console.log(`Express server running on port ${port}`);
